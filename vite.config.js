4607d7c7bb722a44d1c829a55abc5c60d330470e
cezary17 <cezarykujath@gmail.com>
2022-11-26 11:26:39 +0100

    copied cname over

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cezary17.github.io/',
+  base: '/',
   plugins: [svelte()]
 })
